b545631da7c266b0676a40d85908bfb5fef182d6
Alex <alex347a@gmail.com>
2025-04-19 23:23:53 +0200

    Cmake og projektmapper lavet med en main.cpp der påviser build virker

---
+++ build/CMakeFiles/HeroGame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HeroGame.dir/src/main.cpp.o"
+  "CMakeFiles/HeroGame.dir/src/main.cpp.o.d"
+  "HeroGame"
+  "HeroGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HeroGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Let's get ready to rumble!" << std::endl;
+    return 0;
+}
+
