91a40e94be4d9ee8664c0a487c4ea282f846a533
Alex <alex347a@gmail.com>
2025-04-20 16:51:04 +0200

    Fjende headerfil med arv fra Karakter

---
+++ include/Fjende.h
@@ -0,0 +1,18 @@
+#ifndef FJENDE_H
+#define FJENDE_H
+
+#include "Karakter.h"
+
+using namespace std;
+
+class Fjende : public Karakter {
+private:
+    int xpGevinst;
+
+public:
+    Fjende(string navn, int hp, int styrke, int xpGevinst);
+
+    int hentXPGevinst() const;
+};
+
+#endif
