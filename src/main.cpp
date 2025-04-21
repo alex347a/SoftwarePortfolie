96d57c9a48a5dac779a646d3e92242b909a9ae6e
Alex <alex347a@gmail.com>
2025-04-21 15:56:40 +0200

    main.cpp lavet om til at virke med den nye GameManager

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,10 @@
-#include "Hero.h"
-#include "Fjende.h"
+#include "GameManager.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    Hero helt("Alex");
-    Fjende fjende("Testfjende", 3, 1, 1001);
-
-    cout << helt.hentNavn() << " kæmper mod " << fjende.hentNavn() << endl;
-
-    while (helt.erILive() && fjende.erILive()) {
-        cout << helt.hentNavn() << " ramte " << fjende.hentNavn() << " for " << helt.hentStyrke() << " skade" << endl;
-        fjende.tagSkade(helt.hentStyrke());
-        if (fjende.erILive()) {
-            cout << fjende.hentNavn() << " har " << fjende.hentHP() << " HP tilbage." << endl;
-            helt.tagSkade(fjende.hentStyrke());
-            cout << fjende.hentNavn() << " ramte " << helt.hentNavn() << " for " << fjende.hentStyrke() << " skade" << endl;
-            cout << helt.hentNavn() << " har " << helt.hentHP() << " HP tilbage." << endl;
-        }
-    }
-
-    if (helt.erILive()) {
-        cout << helt.hentNavn() << " vandt og får " << fjende.hentXPGevinst() << " XP" << endl;
-        helt.givXP(fjende.hentXPGevinst());
-        if (helt.levelOp()) {
-            cout << "Du er steget i level!" << endl;
-        }
-        cout << "Dit nuværende level er: " << helt.hentLevel() << endl;
-        cout << "Din nuværende XP er: " << helt.hentXP() << endl;
-    } else {
-        cout << helt.hentNavn() << " tabte..." << endl;
-    }
-
+    GameManager spil;
+    spil.start();
     return 0;
 }
-
